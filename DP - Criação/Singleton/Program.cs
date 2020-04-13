9e40727c23b95222f6cf6584a83eab76711ca6c2
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-04-13 13:51:40 -0300

    fix: singleton

---
+++ DP - Criação/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador_1 = Singleton.GetInstancia;
+            jogador_1.Mensagem("jogador 1: A bola está comigo no meio do campo");
+
+            Singleton jogador_2 = Singleton.GetInstancia;
+            jogador_2.Mensagem("jogador 2: Recebeu a bola");
+
+            Singleton jogador_3 = Singleton.GetInstancia;
+            jogador_3.Mensagem("jogador 3: Recebeu o lançamento na linha de fundo");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DP - Criação/Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+        
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if(instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
