1ffea614d6cf0da763121b4325f38f73e715158f
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-06-10 23:34:26 -0300

    Feature: Bridge

---
+++ DP - Estrutura/Bridge/Forma1.cs
@@ -0,0 +1,13 @@
+﻿namespace Bridge
+{
+    public class Forma1 : IForma
+    {
+        public ICor ICor { get; set; }
+
+        public string Descer()
+        {
+            return "T - " + ICor.Cor();
+        }
+
+    }
+}
---
+++ DP - Estrutura/Bridge/IForma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    public interface IForma
+    {
+        string Descer();
+        ICor ICor { get; set; }
+    }
+}
