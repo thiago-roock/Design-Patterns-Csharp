4a4ebcef46f38a4a0389b34b32f4c87972449e1e
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-06-16 22:52:49 -0300

    Feature: Facade

---
+++ DP - Estrutura/Facade/Facade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class Facade
+    {
+        private SubSistemaUm um;
+        private SubSistemaDois dois;
+        private SubSistemaTres tres;
+
+        public Facade()
+        {
+            this.um = new SubSistemaUm();
+            this.dois = new SubSistemaDois();
+            this.tres = new SubSistemaTres();
+        }
+
+        public void OperacaoA()
+        {
+            Console.WriteLine("\nOperação A ------ ");
+            this.um.Responsabilidade();
+            this.dois.Responsabilidade();
+        }
+
+        public void OperacaoB()
+        {
+            Console.WriteLine("\nOperação B ------ ");
+            this.tres.Responsabilidade();
+        }
+    }
+}
---
+++ DP - Estrutura/Facade/SubSistemaUm.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Facade
+{
+    public class SubSistemaUm
+    {
+        public void Responsabilidade()
+        {
+            Console.WriteLine("Coletar Recursos de Energia para a base");
+        }
+    }
+}
