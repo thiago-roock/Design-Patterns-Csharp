0d7fd299f01d0e9ec61af9c5481ada6c8f741774
rdpodcasting <remediosdemaispodcast@gmail.com>
2020-07-18 16:04:36 -0300

    up all comportamento

---
+++ DP-Comportamento2/State/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MyProject
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Template de projeto:"));
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("CONSOLE"));
+
+            Console.Write("\nDigite qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
